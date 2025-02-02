0f8a6ceae7da77e245a186b062b6c70e56bbfa4e
Radu Topala <rtopala@sessionm.com>
2025-02-02 14:03:08 +0200

    dhw setpoint update

--- custom_components/ecoforest_ecogeo/overrides/api.py
+++ custom_components/ecoforest_ecogeo/overrides/api.py
@@ -195,7 +195,7 @@ MAPPING = {
     "number_dhw_setpoint": {
         "data_type": DataTypes.Register,
         "type": "float",
-        "address": 17,
+        "address": 40,
         "entity_type": "temperature",
         "is_number": True,
         "min": 0,
